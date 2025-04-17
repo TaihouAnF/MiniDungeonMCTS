97875984e335528caf7b156f1b086284c55df449
Taihou <ansonfeng1999@gmail.com>
2025-04-16 23:54:16 -0600

    Add DungeonManager/GameManager

--- Assets/Scripts/Core/DungeonManager.cs
+++ Assets/Scripts/Core/DungeonManager.cs
@@ -2,17 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DungeonManager : MonoBehaviour
+public class DungeonManager : MonoBehaviour // Could make a monosingleton here but for the simplicity we are not going to make it
 {
-    // Start is called before the first frame update
-    void Start()
+    public static DungeonManager Instance { get; private set;}  
+    public TileType[,] mp;
+
+    public int Width { get; private set; }
+    public int Height { get; private set;}
+
+    void Awake()
     {
-        
+        Instance = this;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void InitMap(int width, int height) 
+    {
+        Width = width;
+        Height = height;
+        mp = new TileType[Width, Height];
+    }
+
+    public TileType GetTile(int x, int y) 
+    {
+        return x < 0 || y < 0 || x >= Width || y >= Height ? TileType.Empty : mp[x, y];
+    }
+
+    public void SetTile(int x, int y, TileType tile) 
+    {
+        if (x < 0 || y < 0 || x >= Width || y >= Height) return;
+        mp[x, y] = tile;
     }
 }
